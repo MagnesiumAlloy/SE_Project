d865bfd45ede0a319fe137a8a49cb325c204ca5d
lush <2200680109@qq.com>
2022-10-26 18:06:45 +0800

    10.26 1

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"SE_Project/pkg/handler"
-	"SE_Project/pkg/model"
 	"SE_Project/router"
-
-	"golang.org/x/crypto/bcrypt"
 )
 
 func main() {
-	handler.GetDb().AutoMigrate(&model.User{})
-	pwd, _ := bcrypt.GenerateFromPassword([]byte("123"), bcrypt.DefaultCost)
-	handler.GetDb().Create(&model.User{UserName: "user", Password: string(pwd), UserType: model.NormalUserType})
-	pwd, _ = bcrypt.GenerateFromPassword([]byte("admin"), bcrypt.DefaultCost)
-	handler.GetDb().Create(&model.User{UserName: "admin", Password: string(pwd), UserType: model.AdminType})
+	//handler.GetDb().AutoMigrate(&model.User{})
+	//pwd, _ := bcrypt.GenerateFromPassword([]byte("123"), bcrypt.DefaultCost)
+	//handler.GetDb().Create(&model.User{UserName: "user", Password: string(pwd), UserType: model.NormalUserType})
+	//pwd, _ = bcrypt.GenerateFromPassword([]byte("admin"), bcrypt.DefaultCost)
+	//handler.GetDb().Create(&model.User{UserName: "admin", Password: string(pwd), UserType: model.AdminType})
 
 	r := router.SetupRouter()
 	// Listen and Server in 0.0.0.0:8080
