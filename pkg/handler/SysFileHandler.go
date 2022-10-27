d58d700b1f6bc1ea172294bf8856f09955182c6b
lush <2200680109@qq.com>
2022-10-27 20:03:30 +0800

    10.27 20:00

---
+++ pkg/handler/SysFileHandler.go
@@ -0,0 +1,129 @@
+package handler
+
+import (
+	"SE_Project/pkg/model"
+	"SE_Project/pkg/util"
+	"crypto/md5"
+	"encoding/hex"
+	"errors"
+	"io"
+	"log"
+	"os"
+	"syscall"
+	"time"
+)
+
+var err error
+
+func SysCheckIsDir(path string) error {
+	folderinfo, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	//log.Println(folderinfo)
+	//check is a dir
+	if !folderinfo.IsDir() {
+		return errors.New("target is not a dir")
+	}
+	return nil
+}
+
+func SysCheckFileExist(path string) error {
+	_, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func SysReadDir(path string) ([]model.Data, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	files, err := f.ReadDir(0)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	var result []model.Data
+	for _, file := range files {
+		log.Println(file)
+		info, err := file.Info()
+		if err != nil {
+			return nil, err
+		}
+		result = append(result, model.Data{
+			Name: file.Name(),
+			Type: util.GetTargetType(file.Name(), file.IsDir()),
+			Size: uint64(info.Size()),
+			Path: ""})
+
+	}
+	return result, nil
+}
+
+func SysReadFileInfo(name, path string) (*model.Data, error) {
+	res := model.Data{Name: name, Path: path}
+	if err := SysCheckIsDir(name + "/" + path); err != nil {
+		res.Type = util.GetTargetType(name, false)
+	} else {
+		res.Type = model.Dir
+	}
+	info, err := os.Stat(name + "/" + path)
+	if err != nil {
+		return nil, err
+	}
+	res.Size = uint64(info.Size())
+	res.ModTime = info.ModTime()
+	res.CreatTime = time.Unix(info.Sys().(*syscall.Stat_t).Ctim.Sec, 0)
+	return &res, nil
+}
+
+func SysCopy(srcPath, desPath string, modTime time.Time, CreatTime time.Time) error {
+	srcFile, err := os.Open(srcPath)
+	if err != nil {
+		return err
+	}
+	desFile, err := os.OpenFile(desPath, os.O_WRONLY|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	_, err = io.Copy(desFile, srcFile)
+	defer desFile.Close()
+	defer srcFile.Close()
+	return err
+}
+
+func SysGetFileMd5(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	md5h := md5.New()
+	io.Copy(md5h, file)
+
+	return hex.EncodeToString(md5h.Sum(nil)), nil
+}
+
+func SysCompare(srcPath, desPath string) error {
+	var srcMd5, desMd5 string
+	if srcMd5, err = SysGetFileMd5(srcPath); err != nil {
+		return err
+	}
+	if desMd5, err = SysGetFileMd5(srcPath); err != nil {
+		return err
+	}
+	if srcMd5 != desMd5 {
+		return errors.New("not the same")
+	}
+	return nil
+}
+
+func SysCleanFile(path string) error {
+	if err := os.Remove(path); err != nil {
+		return err
+	}
+	return nil
+}
--- pkg/service/FileService.go
+++ pkg/service/FileService.go
@@ -4,65 +4,30 @@ import (
 	"SE_Project/pkg/handler"
 	"SE_Project/pkg/model"
 	"SE_Project/pkg/util"
-	"errors"
-	"log"
 	"os"
 	"path/filepath"
+	"time"
 )
 
-func SysCheckIsDir(path string) error {
-	folderinfo, err := os.Stat(path)
-	if err != nil {
-		return err
-	}
-	//log.Println(folderinfo)
-	//check is a dir
-	if !folderinfo.IsDir() {
-		return errors.New("target is not a dir")
-	}
-	return nil
-}
+var err error
+var obj *model.Data
 
-func SysReadDir(path string) ([]model.Data, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	files, err := f.ReadDir(0)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	var result []model.Data
-	for _, file := range files {
-		log.Println(file)
-		info, err := file.Info()
-		if err != nil {
-			return nil, err
-		}
-		result = append(result, model.Data{
-			Name: file.Name(),
-			Type: util.GetTargetType(file.Name(), file.IsDir()),
-			Size: uint64(info.Size()),
-			Path: ""})
-
-	}
-	return result, nil
-}
-
-func CheckIsDir(path string, isRoot bool, isBin bool) error {
+func checkIsDir(path string, isRoot bool, isBin bool) error {
 	if isRoot {
-		if err := handler.NewFileHandler(&model.Data{Path: path, IsDeleted: isBin}).CheckIsDir(); err != nil {
+		if err := handler.NewFileHandler(&model.Data{Path: path, IsDeleted: isBin, Type: model.Dir}).CheckTargetExist(); err != nil {
 			return err
 		}
 		return nil
 	} else {
-		return SysCheckIsDir(path)
+		return handler.SysCheckIsDir(path)
 	}
 
 }
 
 func ReadDir(path string, isRoot bool, isBin bool) ([]model.Data, error) {
+	if err := checkIsDir(path, isRoot, isBin); err != nil {
+		return nil, err
+	}
 	if isRoot {
 		result, err := handler.NewFileHandler(&model.Data{Path: path, IsDeleted: isBin}).ReadDir()
 		if err != nil {
@@ -70,7 +35,7 @@ func ReadDir(path string, isRoot bool, isBin bool) ([]model.Data, error) {
 		}
 		return result, nil
 	} else {
-		return SysReadDir(path)
+		return handler.SysReadDir(path)
 	}
 }
 
@@ -117,30 +82,114 @@ func ReadAllFileAndDir(root string) ([]model.Data, error) {
 	return res, nil
 }
 
+func checkFileExist(name, path string, params ...bool) error {
+	isRoot := params[0]
+	if isRoot {
+		isBin := false
+		if len(params) > 1 {
+			isBin = params[1]
+		}
+		if err := handler.NewFileHandler(&model.Data{Name: name, Path: path, IsDeleted: isBin}).CheckTargetExist(); err != nil {
+			return err
+		}
+	} else {
+		return handler.SysCheckFileExist(path + "/" + name)
+	}
+	return nil
+}
 func Recover(srcName, srcPath, desPath string) error {
+	if err = checkFileExist(srcName, srcPath, true); err != nil {
+		return err
+	}
+	if obj, err = handler.NewFileHandler(&model.Data{Name: srcName, Path: srcName, IsDeleted: false}).GetTarget(); err != nil {
+		return err
+	}
+	if err = handler.SysCopy(model.Root+obj.Path+"/"+obj.Name, desPath+"/"+obj.Name, obj.ModTime, obj.CreatTime); err != nil {
+		return err
+	}
 	return nil
 }
 
 func Compare(srcName, srcPath, desName, desPath string) error {
+	if err := checkFileExist(srcName, srcPath, false); err != nil {
+		return err
+	}
+	if err := checkFileExist(desName, desPath, true); err != nil {
+		return err
+	}
+	if obj, err = handler.NewFileHandler(&model.Data{Name: desName, Path: desName, IsDeleted: false}).GetTarget(); err != nil {
+		return err
+	}
+	if err = handler.SysCompare(srcPath+"/"+srcPath, model.Root+obj.Path+"/"+obj.Name); err != nil {
+		return err
+	}
 	return nil
 }
 
-func Delete(Name, Path string) error {
+func Delete(name, path string) error {
+	if err := checkFileExist(name, path, true); err != nil {
+		return err
+	}
+	if err := handler.NewFileHandler(&model.Data{Name: name, Path: path, Type: model.Dir}).CheckTargetExist(); err == nil {
+		if err := handler.NewFileHandler(&model.Data{Path: path + "/" + name}).Delete(); err != nil {
+			return err
+		}
+	}
+	if err := handler.NewFileHandler(&model.Data{Name: name, Path: path}).Delete(); err != nil {
+		return err
+	}
 	return nil
 }
 
 func Backup(srcName, srcPath, desPath string) error {
+	if err := checkFileExist(srcName, srcName, false); err != nil {
+		return err
+	}
+	if obj, err = handler.SysReadFileInfo(srcName, srcPath); err != nil {
+		return err
+	}
+
+	if err := handler.NewFileHandler(obj).Backup(); err != nil {
+		return err
+	}
+	if err := handler.SysCopy(srcPath+"/"+srcName, model.Root+desPath, time.Now(), time.Now()); err != nil {
+		return err
+	}
+
 	return nil
 }
 
 func BackupWithKey(srcName, srcPath, desPath, key string) error {
+	if err := checkFileExist(srcName, srcName, false); err != nil {
+		return err
+	}
+	//todo
 	return nil
 }
 
 func Clean(name, path string) error {
+	if err := checkFileExist(name, path, true, true); err != nil {
+		return err
+	}
+	if err := handler.NewFileHandler(&model.Data{Name: name, Path: path}).Clean(); err != nil {
+		return err
+	}
+	if err := handler.SysCleanFile(model.Bin + path + "/" + name); err != nil {
+		return err
+	}
 	return nil
 }
 
 func Recycle(name, path string) error {
+	if err := checkFileExist(name, path, true, true); err != nil {
+		return err
+	}
+	if err := handler.NewFileHandler(&model.Data{Name: name, Path: path}).Recycle(); err != nil {
+		return err
+	}
+
+	if err := handler.SysCopy(model.Bin+path+"/"+name, model.Root+path+"/"+name, time.Now(), time.Now()); err != nil {
+		return err
+	}
 	return nil
 }
