2aa782d7f0a15d09aa4c02f396108457185929ec
Yezouhua <1069812938@qq.com>
2022-10-29 22:26:55 +0800

    lxk 10/29 22.00

--- web/static/js/myfile.js
+++ web/static/js/myfile.js
@@ -1,113 +1,3 @@
-function user1(){
-    $.ajax({
-        //几个参数需要注意一下
-        dataType: "json",
-        url: "/User1" ,
-        type: "POST", 
-        data: $('#user1').serialize(),
-        success: function (result) {
-            console.log(result);//打印服务端返回的数据(调试用)
-            if (result.status == 200) {
-               location.href="/myfile";
-            } else {
-                alert("错误");
-            }
-        },
-        error : function() {
-            console.log("failed");
-            alert("异常！");
-        }
-    });
-}
-
-
-function user4(){
-    $.ajax({
-        //几个参数需要注意一下
-        dataType: "json",
-        url: "/User4" ,
-        type: "POST", 
-        data: $('#user4').serialize(),
-        success: function (result) {
-            console.log(result);//打印服务端返回的数据(调试用)
-            if (result.status == 200) {
-               location.href="recycle";
-            } else {
-                alert("错误");
-            }
-        },
-        error : function() {
-            console.log("failed");
-            alert("异常！");
-        }
-    });
-}
-
-function user5(){
-    $.ajax({
-        //几个参数需要注意一下
-        dataType: "json",
-        url: "/User5" ,
-        type: "POST", 
-        data: $('#user5').serialize(),
-        success: function (result) {
-            console.log(result);//打印服务端返回的数据(调试用)
-            if (result.status == 200) {
-               location.href="share";
-            } else {
-                alert("错误");
-            }
-        },
-        error : function() {
-            console.log("failed");
-            alert("异常！");
-        }
-    });
-}
-
-function user6(){
-    $.ajax({
-        //几个参数需要注意一下
-        dataType: "json",
-        url: "/User6" ,
-        type: "POST", 
-        data: $('#user6').serialize(),
-        success: function (result) {
-            console.log(result);//打印服务端返回的数据(调试用)
-            if (result.status == 200) {
-               location.href="/secret";
-            } else {
-                alert("错误");
-            }
-        },
-        error : function() {
-            console.log("failed");
-            alert("异常！");
-        }
-    });
-}
-
-function user2(){ //私密上传 函数要重新写
-    console.log(123)
-    const fle = document.getElementById('fileUpload')
-    console.log('111',fle)
-     fle.click()
-    console.log(123)
-    console.log($("#fileUpload")[0].files);
-    upload()
-}
-
-function user3(){ //普通上传 函数要重新写
-  console.log(123)
-  const fle = document.getElementById('fileUpload')
-  console.log('111',fle)
-   fle.click()
-  console.log(123)
-  console.log($("#fileUpload")[0].files);
-  upload()
-}
-
-
 function upload(){
   $.ajax({
         //几个参数需要注意一下
@@ -153,59 +43,83 @@ function getFile(value){
 
 }
 
-  function my(){
-       $.ajax({
-           url: "/fileData",
-           type:"get",
-           dataType:"json",
-           success:function (data) {
-               console.log(data);
-               myFunction(data.data);
-           }
-       })};
-
-  function myFunction(arr) {
-      var out = "";
-      for(let i = 0; i < arr.length; i++) {
-          out += "<tr  onclick="+"edit(this)"+">"
-                   +"<td>"+arr[i].ID+"</td>"
-                   +"<td>"+arr[i].Name+"</td>"
-                   +"<td>"+arr[i].Name+"</td>"
-                   +"<td>"+arr[i].Path+"</td>"
-                   +"<td>"+arr[i].Type+"</td>"
-                +"</tr>";
-      }
-      document.getElementById("content").innerHTML=out;
-  }
-
-  function myy(filepath){
-       $.ajax({
-           url: "/innerData",
-           type:"get",
-           dataType:"json",
-           data: {
-            filepath:filepath
-           },
-           success:function (res) {
-               console.log(res);
-               myFunction(res.data);
-           }
-       })};
-    
-
-    function edit(obj){
-            let node=obj;
-            let id=node.children[0].innerHTML;
-            let filetype=node.children[4].innerHTML;
-            let filepath=node.children[3].innerHTML;
-            //alert(id,filepath,filetype);
-            if(filetype == "t1"){ //文件夹类型,要你指定
-              //更进一步
-              myy(filepath);
-            } else {
-              let f=document.forms[0];
-              f.action="/search?id="+id;
-              f.submit();
-            }
+function Stack(){
+    this.items = [];
+    // 添加一个新元素到栈顶位置。push()
+    Stack.prototype.push = function(element){
+       this.items.push(element);
+    }
+     // 移除栈顶的元素pop()
+    Stack.prototype.pop = function(){
+        return this.items.pop();
+    }
+    //  返回栈顶的元素，不对栈做任何修改peek()
+    Stack.prototype.peek = function(){
+        return this.items[this.items.length-1];
+    }
+    // 判断栈是否空isEmpty()
+    Stack.prototype.isEmpty = function(){
+        if(this.items.length == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    // 返回栈里的元素个数size()
+    Stack.prototype.size = function(){
+        return this.items.length;
+    }
+    // 将栈结构的内容以字符形式返回toString()
+    Stack.prototype.toString = function(){
+        var str = '';
+        for(var i =0;i<this.items.length;i++){
+            str += this.items[i] + ' ';
+        }
+        return str;
      }
+   }
+      
 
+    function readDir(isroot,ID,path,inBin) {
+    $.ajax({
+      url: "/readDir",
+      type: "GET",
+      dataType: "json",
+      data: {
+        path: path,
+        ID: ID,
+        isroot: isroot,
+        inBin: inBin
+      },
+      success: function (res) {
+        IsRoot=isroot
+        Path = path
+        InBin=inBin;
+        document.getElementById('tmpPath').value=Path;
+        showTable(res.data,isroot,res.ID,res.Path,inBin);
+      }
+    })
+  };
+
+  function ReadDirWithPop(){
+    let isroot=IsRoot;
+    let filepath=Path;
+    let inBin=InBin;
+    if(stackID.size()<=1 || stackPath.size()<=1){
+      return;
+    }
+    stackID.pop();
+    stackPath.pop();
+    readDir(isroot,stackID.peek(),stackPath.peek(),inBin);
+  };
+
+  function ReadDirWithPush(isroot,id,path,inBin){
+    stackID.push(id);
+    stackPath.push(path);
+    readDir(isroot,id,path,inBin);
+  };
+
+  function rTime(date) {
+      var json_date = new Date(date).toJSON();
+      return new Date(+new Date(json_date) + 8 * 3600 * 1000).toISOString().replace(/T/g, ' ').replace(/\.[\d]{3}Z/, '')
+  }
