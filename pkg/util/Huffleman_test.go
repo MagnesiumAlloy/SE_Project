21786843f885ab5262ff726db4a3ce142feb102c
lush <2200680109@qq.com>
2022-10-31 21:12:04 +0800

    10.31 t4

--- pkg/util/Huffleman_test.go
+++ pkg/util/Huffleman_test.go
@@ -3,11 +3,11 @@ package util
 import "testing"
 
 func TestCompress(t *testing.T) {
-	if res, err := Compress("/home/lush/SE_Project/Input.pack"); err != nil {
+	if res, err := Compress("/home/lush/tmp/ccloud.zip"); err != nil {
 		println(err, res)
 	}
 
-	if res, err := Decompress("/home/lush/SE_Project/Input.pack_tmp_"); err != nil {
+	if res, err := Decompress("/home/lush/tmp/ccloud.zip_tmp_"); err != nil {
 		println(err, res)
 	}
 }
